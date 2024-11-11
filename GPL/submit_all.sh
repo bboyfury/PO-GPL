15ba0d1365fb5041a64a702c4467d25bb58dea84
Alireza Saleh Abadi <36476023+bboyfury@users.noreply.github.com>
2024-11-11 17:14:44 -0600

    minor change

--- GPL/submit_all.sh
+++ GPL/submit_all.sh
@@ -7,11 +7,30 @@ num_particles=("1" "5" "10" "20")
 
 # Loop through each environment, experiment name, and particle count
 for env in "${environments[@]}"; do
+
+    # Map each environment to the desired env_base_name
+    case $env in
+        ("PO-Adhoc-Foraging-2-12x12-3f-v0")
+            env_base_name="LBF"
+            ;;
+        ("Adhoc-wolfpack-v5")
+            env_base_name="Wolfpack"
+            ;;
+        ("PO-Navigation-2-12x12-v1")
+            env_base_name="CooperativeNavigation"
+            ;;
+        ("fortattack-v0")
+            env_base_name="FortAttack"
+            ;;
+        (*)
+            env_base_name=$(echo $env | sed 's/-/_/g')  # Fallback if a new environment is added
+            ;;
+    esac
+
     for exp in "${exp_names[@]}"; do
         for particles in "${num_particles[@]}"; do
             
             # Set dynamic saving and logging directories
-            env_base_name=$(echo $env | sed 's/-/_/g')  # Replace '-' with '_' for naming
             bash_file_name=$(basename "$0" .sh)         # Get the name of the current script without .sh
             saving_dir="param_${env_base_name}_${bash_file_name}"
             logging_dir="logs_${env_base_name}_${bash_file_name}"
@@ -21,8 +40,8 @@ for env in "${environments[@]}"; do
             sbatch --job-name="${env_base_name}_${exp}_${particles}" run_vae_d.sh "$env" "$exp" "$particles" "$logging_dir" "$saving_dir"
         
         done
-            sbatch --job-name="${env_base_name}_${exp}_${particles}" run_gpl_d.sh "$env"  "$exp" "$logging_dir" "$saving_dir"
-            sbatch --job-name="${env_base_name}_${exp}_${particles}" run_ae_d.sh "$env"  "$exp" "$logging_dir" "$saving_dir"
-
+        # Submit jobs that don’t use particle parameters
+        sbatch --job-name="${env_base_name}_${exp}_gpl" run_gpl_d.sh "$env" "$exp" "$logging_dir" "$saving_dir"
+        sbatch --job-name="${env_base_name}_${exp}_ae" run_ae_d.sh "$env" "$exp" "$logging_dir" "$saving_dir"
     done
 done
